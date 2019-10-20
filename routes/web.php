56542fa8f61f3f9ee3e0cf63d76017fa32477065
weilaim <879181054@qq.com>
2019-10-21 04:50:40 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
+//个人页面
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
