04682af9852efc07b7aaf619b16f19b5bcf042e9
weilaim <879181054@qq.com>
2019-10-21 07:45:19 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//根据分类列表话题
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
