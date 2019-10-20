26ab2217bf9e621c7a1cdd9f6da96d12b591c229
weilaim <879181054@qq.com>
2019-10-21 06:39:59 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,4 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 //个人页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
