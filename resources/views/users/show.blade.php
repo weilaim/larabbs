85aa5db7cd540a7769f32305c1ce504ccfed76e5
weilaim <879181054@qq.com>
2019-10-21 05:25:03 +0800

    编辑个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,10 +11,10 @@
                 <img class="card-img-top" src="https://cdn.learnku.com/uploads/avatars/47004_1565510685.jpeg!/both/100x100" alt="{{ $user->name }}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <p>{{  $user->introduction }}</p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>January 01 1901</p>
+                    <p>{{  $user->created_at}}</p>
                 </div>
             </div>
         </div>
