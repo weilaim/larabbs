e233de86608cb61937770dfd7d3cb811cdd4e9c4
weilaim <879181054@qq.com>
2019-10-21 02:35:17 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="text-center">
+            Copyright ©2019 weilaim.top <a href="http://www.beian.miit.gov.cn/" target="_blank">桂ICP备19004198号-1</a>
+            </a>
+        </p>
+    </div>
+</footer>
