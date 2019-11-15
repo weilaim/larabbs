1471d32d2aefb1deed990b7021e5af533f17d664
weilaim <879181054@qq.com>
2019-11-15 09:35:25 +0800

    用户可以传出话题图片

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,6 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 //根据分类列表话题
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+//upload img
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
