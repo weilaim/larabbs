de63aacc1d754568d344a1878a8680176e62b288
weilaim <879181054@qq.com>
2019-11-16 09:35:00 +0800

    翻译队列话

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -26,7 +26,7 @@ class TopicsController extends Controller
     public function show(Request $request,Topic $topic)
     {
         //url矫正
-        if(!empyt($topic->slug) && $topic->slug != $request->slug){
+        if(!empty($topic->slug) && $topic->slug != $request->slug){
             return redirect($topic->link(),301);
         }
         return view('topics.show', compact('topic'));
