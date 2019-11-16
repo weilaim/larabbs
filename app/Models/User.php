405e2aa13946ea8b0daf36037511235c6530ab41
weilaim <879181054@qq.com>
2019-11-16 17:34:22 +0800

    消息通知有新回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,28 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
-
+use Auth;
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable, MustVerifyEmailTrait;
+    use MustVerifyEmailTrait;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance){
+        //如果通知的是当前用户，就不必通知了
+        if($this->id == Auth::id()){
+            return;
+        }
+
+        //只有数据库类型通知才需提醒，直接发送Email 或者其他的都pass
+        if(method_exists($instance,'toDatabases')){
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
 
     protected $fillable = [
         'name', 'email', 'password','introduction','avatar',
